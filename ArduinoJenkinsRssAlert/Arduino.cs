73453cd130dca3abacada2ec2f857542dbc80bf5
Jean Bastos <Jean Bastos>
2017-09-07 21:28:04 -0300

    Add project files.

---
+++ ArduinoJenkinsRssAlert/Arduino.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ArduinoJenkinsRssAlert
+{
+    public class Arduino
+    {
+        SerialPort _serial;
+
+        public Arduino(string nomePorta)
+        {
+            _serial = new SerialPort();
+            _serial.PortName = nomePorta;
+        }
+
+        public void Abrir()
+        {
+            _serial.Open();
+        }
+
+        public void Fechar()
+        {
+            _serial.Close();
+        }
+
+        public void Inicializar()
+        {
+            Sucesso();
+        }
+
+        public void Sucesso()
+        {
+            Abrir();
+            Enviar(250);
+            Thread.Sleep(500);
+            Enviar(1);
+            Thread.Sleep(500);
+            Fechar();
+        }
+
+        public void Falha()
+        {
+            Abrir();
+            Enviar(200);
+            Thread.Sleep(500);
+            Enviar(180);
+            Thread.Sleep(500);
+            Fechar();
+        }
+
+        private void Enviar(int graus)
+        {
+            if (_serial.IsOpen)
+            {
+                _serial.WriteLine(graus.ToString());
+            }
+        }
+    }
+}
---
+++ ArduinoJenkinsRssAlert/Program.cs
@@ -0,0 +1,55 @@
+﻿using ArduinoJenkinsRssAlert.Modelo.Atom;
+using System;
+using System.Threading;
+
+namespace ArduinoJenkinsRssAlert
+{
+    class Program
+    {
+        static bool situacaoAtualQuebrado = false;
+        static Arduino _arduino;
+
+        static void Main(string[] args)
+        {
+            //https://ci.jenkins.io/rssFailed
+            //https://ci.jenkins.io/rssLatest
+
+            var cancelar = new CancellationTokenSource();
+
+            var monitor = new Monitor();
+            monitor.ReceberFeeds += Receber;
+            monitor.IniciarAsync(cancelar.Token, args[0]);
+
+            _arduino = new Arduino(args[1]);
+            _arduino.Inicializar();
+            
+            Console.Read();
+            cancelar.Cancel();
+        }
+
+        static void Receber(Feed feed)
+        {
+            // Verifica se a situação mudou...
+            var quebrou = feed.Entry.Count > 0;
+
+            if (situacaoAtualQuebrado == quebrou)
+            {
+                return;
+            }
+            situacaoAtualQuebrado = quebrou;
+
+            if (quebrou)
+            {
+                foreach (var item in feed.Entry)
+                {
+                    Console.Write(item.Title);
+                }
+                _arduino.Falha();
+            }
+            else
+            {
+                _arduino.Sucesso();
+            }
+        }
+    }
+}
