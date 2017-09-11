55da5e145df30b342c42dd115847dc1d2cd5be40
Jean Bastos <jean.bastos@senior.com.br>
2017-09-11 11:24:22 -0300

    Add project files.

---
+++ ArduinoJenkinsRssAlert/Arduino.cs
@@ -0,0 +1,72 @@
+﻿using System.IO.Ports;
+using System.Threading;
+
+namespace ArduinoJenkinsRssAlert
+{
+    public class Arduino
+    {
+        private SerialPort _serial;
+
+        public Arduino(string nomePorta)
+        {
+            _serial = new SerialPort();
+            _serial.PortName = nomePorta;
+        }
+
+        public void Abrir()
+        {
+            _serial.Open();
+        }
+
+        public void Fechar()
+        {
+            _serial.Close();
+        }
+
+        public void Inicializar()
+        {
+            Sucesso();
+        }
+
+        public void Sucesso()
+        {
+            try
+            {
+                Abrir();
+                Enviar(250);
+                Thread.Sleep(500);
+                Enviar(1);
+                Thread.Sleep(500);
+            }
+            finally
+            {
+                Fechar();
+            }
+        }
+
+        public void Falha()
+        {
+            try
+            {
+                Abrir();
+                Enviar(200);
+                Thread.Sleep(500);
+                Enviar(180);
+                Thread.Sleep(500);
+                Fechar();
+            }
+            finally
+            {
+                Fechar();
+            }
+        }
+
+        private void Enviar(int graus)
+        {
+            if (_serial.IsOpen)
+            {
+                _serial.WriteLine(graus.ToString());
+            }
+        }
+    }
+}
---
+++ ArduinoJenkinsRssAlert/Monitor.cs
@@ -0,0 +1,48 @@
+﻿using ArduinoJenkinsRssAlert.Modelo.Atom;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ArduinoJenkinsRssAlert
+{
+    public class Monitor
+    {
+        public delegate void FeedDelegate(Feed Feed);
+        public event FeedDelegate ReceberFeeds;
+
+        /// <summary>
+        /// Inicia monitoramento
+        /// </summary>
+        public async void IniciarAsync(CancellationToken token, string url)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var feed = await AnalizarAsync(url);
+                ReceberFeeds?.Invoke(feed);
+
+                Thread.Sleep(5000);
+            }
+        }
+
+        /// <summary>
+        /// Busca jobs quebrados
+        /// </summary>
+        private async Task<Feed> AnalizarAsync(string url)
+        {
+            var cliente = new HttpClient();
+            var feedXml = await cliente.GetStringAsync(url);
+            var buffer = Encoding.UTF8.GetBytes(feedXml);
+
+            var serializador = new XmlSerializer(typeof(Feed));
+            using (var leitor = new MemoryStream(buffer))
+            {
+                var feed = (Feed)serializador.Deserialize(leitor);
+
+                return feed;
+            }
+        }
+    }
+}
